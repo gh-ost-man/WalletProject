612e84998408c26a38b1e1b9d49b607b1e774795
Mark Brighton <marik.shevchenko@icloud.com>
2020-11-13 18:01:14 +0200

    Mark -> зробив бібліотеку, зробив для неї білд. Включив її до проекту. Робив це на своїй гілці.

---
+++ CreditCardLib/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardLib
+{
+    public class CreditCard
+    {
+        public int ID { get; set; } = 7;
+        public string Name { get; set; } = "Albert";
+
+        public override string ToString()
+        {
+            return $"ID: {ID}\n" +
+                $"Name: {Name}";
+        }
+    }
+}
--- Wallet/Program.cs
+++ Wallet/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CreditCardLib;
 
 namespace Wallet
 {
@@ -11,6 +12,7 @@ namespace Wallet
         static void Main(string[] args)
         {
             Console.WriteLine("Aloha");
+            CreditCard creditCard = new CreditCard();            
         }
     }
 }
